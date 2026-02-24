b233caeb4ace7c687c1c09c4a92430ae276bbee0
Rakyrish <mbuguajohn367@gmail.com>
2026-02-24 22:24:45 +0300

    adding database

--- DjangoCrud/settings.py
+++ DjangoCrud/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-$sqk(m)9c!p4$n7lae5v7vfi_z!mh(1y+51bd+b7=83ca_1qle
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.railway.app, "*"']
-
+ALLOWED_HOSTS = ['*', '.railway.app', 'localhost', '127.0.0.1']
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
